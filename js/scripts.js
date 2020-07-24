9c82b14bea9ea8a4b3d5db3ccfd51ba630c30730
diesgo <diesgo@gmail.com>
2020-07-24 19:25:46 +0200

    Estado inicial del proyecto

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+  data: {
+    cursos: [
+    { name: "Fundamentos de React", url: 'http://cursos.carlosazaustre.es/p/react-js' },
+    { name: "Redux con React", url: 'http://cursos.carlosazaustre.es/p/curso-profesional-de-redux-y-react' },
+    { name: "React Native", url: 'http://cursos.carlosazaustre.es/p/react-native' },
+    ]
+  }
+})
