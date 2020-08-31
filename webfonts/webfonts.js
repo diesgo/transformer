989d6e200ba8ff535cd77ad4801bfbe89999c425
diesgo <diesgo@gmail.com>
2020-08-31 02:13:49 +0200

    modificar pagina tipografias

--- webfonts/webfonts.js
+++ webfonts/webfonts.js
@@ -1,20 +1,20 @@
-var fonts = ["amarant", "arima", "catamaran", "de-valencia", "densia", "emblem", "haetten-schweiler", "lemonada", "lobster", "monoton", "montserrat", "nunito", "optima", "oswald", "permanent-maker","restu", "roboto", "samble-tracie", "samble-tracie-swash", "spoken", "fat-wandals","robofan-symbols", "transformers","varela"];
+var fonts = ["amaranth-bold", "arima-madurai", "catamaran", "comfortaa-bold", "confortaa-regular", "de-valencia", "densia", "emblem", "haetten-schweiler", "lemonada", "lobster", "manrope-extralight", "monoton", "montserrat", "nunito", "oswald", "permanent-maker", "pompiere-regular", "restu", "roboto", "samble-tracie", "samble-tracie-swash", "fat-wandals", "spoken", "robofan-symbols", "transformers","varela", "zanzabar"];
+
+var numFonts = fonts.length;
+var x = document.getElementById("body");
 
 function cambioFuente() {
-    var f = document.getElementById("fuentes").selectedIndex;
-    var font = fonts[f];
-    var x = document.getElementById("muestra");
-    if (x.className.indexOf("font-" + font) == -1) {
-        x.setAttribute("class", "font-" + font); 
-    } else {
-        x.classList.remove("font-" + font);
-    }
+    let f = document.getElementById("fuentes").selectedIndex;
+    let font = fonts[f];
+    document.getElementById("muestra").className = "font-" + font;
     document.getElementById("fuente").innerHTML = font;
 }
 
 function cambioFuente2() {
     var f2 = document.getElementById("fuentes2").selectedIndex;
-    var font2 = fonts[f2];
-    document.getElementById("dos").classList.add("font-" + font2);
+    font2 = fonts[f2];
+    document.getElementById("dos").className = "font-" + font2;
     document.getElementById("fuente2").innerHTML = font2;
 }
+cambioFuente();
+cambioFuente2();
